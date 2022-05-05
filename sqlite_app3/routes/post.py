d39008fc8c2fa75fac8fb7d748588e39b93372b4
thr3a <thr3a@example.com>
2022-05-05 21:32:42 +0900

    add:

--- sqlite_app3/routes/post.py
+++ sqlite_app3/routes/post.py
@@ -2,9 +2,12 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from .. import schemas, models, database
-router = APIRouter()
+router = APIRouter(
+  prefix="/posts",
+  tags=["posts"]
+)
 
-@router.post("/posts", tags=["posts"])
+@router.post("/")
 def create_post(request: schemas.CreatePost, db: Session = Depends(database.get_db)):
   new_post = models.Post(**request.dict())
   db.add(new_post)
@@ -12,12 +15,12 @@ def create_post(request: schemas.CreatePost, db: Session = Depends(database.get_
   db.refresh(new_post)
   return new_post
 
-@router.get("/posts", response_model=List[schemas.ShowPostWithUser], tags=["posts"])
+@router.get("/", response_model=List[schemas.ShowPostWithUser])
 def all_fetch(db: Session = Depends(database.get_db)):
   posts = db.query(models.Post).all()
   return posts
 
-@router.get('/posts/{id}', response_model=schemas.ShowPostWithUser, tags=["posts"])
+@router.get('/{id}', response_model=schemas.ShowPostWithUser)
 def show(id: int, db: Session = Depends(database.get_db)):
   post = db.query(models.Post).filter(models.Post.id == id).first()
   if not post:
--- sqlite_app3/routes/user.py
+++ sqlite_app3/routes/user.py
@@ -2,9 +2,12 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from .. import schemas, models, database
-router = APIRouter()
+router = APIRouter(
+  prefix="/users",
+  tags=["users"]
+)
 
-@router.post("/users", response_model=schemas.ShowUser, tags=["users"])
+@router.post("/", response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(database.get_db)):
   hashedPassword = request.password + 'hogehoge'
   new_user = models.User(email=request.email, password=hashedPassword)
@@ -13,19 +16,19 @@ def create_user(request: schemas.User, db: Session = Depends(database.get_db)):
   db.refresh(new_user)
   return new_user
 
-@router.get("/users", response_model=List[schemas.ShowUserWithPosts], tags=["users"])
+@router.get("/", response_model=List[schemas.ShowUserWithPosts])
 def all_fetch(db: Session = Depends(database.get_db)):
   users = db.query(models.User).all()
   return users
 
-@router.get('/users/{id}', response_model=schemas.ShowUserWithPosts, tags=["users"])
+@router.get('/{id}', response_model=schemas.ShowUserWithPosts)
 def show(id: int, db: Session = Depends(database.get_db)):
   user = db.query(models.User).filter(models.User.id == id).first()
   if not user:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
   return user
 
-@router.delete('/users/{id}', tags=["users"])
+@router.delete('/{id}')
 def delete(id: int, db: Session = Depends(database.get_db)):
   user = db.query(models.User).filter(models.User.id == id)
   if not user.first():
@@ -34,7 +37,7 @@ def delete(id: int, db: Session = Depends(database.get_db)):
   db.commit()
   return {"message": "ok"}
 
-@router.put("/users/{id}", response_model=schemas.ShowUserWithPosts, tags=["users"])
+@router.put("/{id}", response_model=schemas.ShowUserWithPosts)
 def update(id: int, request: schemas.User, db: Session = Depends(database.get_db)):
   user = db.query(models.User).filter(models.User.id == id)
   if not user.first():
