eed1f2ed9d74b7f1bf6b14c09c00ea6539f3ed6d
thr3a <thr3a@example.com>
2022-05-05 20:28:36 +0900

    add:

--- sqlite_app2/main.py
+++ sqlite_app2/main.py
@@ -18,7 +18,7 @@ def get_db():
   finally:
     db.close()
 
-@app.post("/users")
+@app.post("/users", response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
   hashedPassword = pwd_cxt.hash(request.password)
   new_user = models.User(email=request.email, password=hashedPassword)
@@ -27,12 +27,12 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
   db.refresh(new_user)
   return new_user
 
-@app.get("/users", response_model=List[schemas.ShowUser])
+@app.get("/users", response_model=List[schemas.ShowUserWithPosts])
 def all_fetch(db: Session = Depends(get_db)):
   users = db.query(models.User).all()
   return users
 
-@app.get('/users/{id}', response_model=schemas.ShowUser)
+@app.get('/users/{id}', response_model=schemas.ShowUserWithPosts)
 def show(id: int, db: Session = Depends(get_db)):
   user = db.query(models.User).filter(models.User.id == id).first()
   if not user:
@@ -48,7 +48,7 @@ def delete(id: int, db: Session = Depends(get_db)):
   db.commit()
   return {"message": "ok"}
 
-@app.put("/users/{id}", response_model=schemas.ShowUser)
+@app.put("/users/{id}", response_model=schemas.ShowUserWithPosts)
 def update(id: int, request: schemas.User, db: Session = Depends(get_db)):
   user = db.query(models.User).filter(models.User.id == id)
   if not user.first():
@@ -58,19 +58,19 @@ def update(id: int, request: schemas.User, db: Session = Depends(get_db)):
   return user.first()
 
 @app.post("/posts")
-def create_post(request: schemas.Post, db: Session = Depends(get_db)):
-  new_post = models.Post(title=request.title, body=request.body, user_id=1)
+def create_post(request: schemas.CreatePost, db: Session = Depends(get_db)):
+  new_post = models.Post(**request.dict())
   db.add(new_post)
   db.commit()
   db.refresh(new_post)
   return new_post
 
-@app.get("/posts", response_model=List[schemas.ShowPost])
+@app.get("/posts", response_model=List[schemas.ShowPostWithUser])
 def all_fetch(db: Session = Depends(get_db)):
   posts = db.query(models.Post).all()
   return posts
 
-@app.get('/posts/{id}', response_model=schemas.ShowPost)
+@app.get('/posts/{id}', response_model=schemas.ShowPostWithUser)
 def show(id: int, db: Session = Depends(get_db)):
   post = db.query(models.Post).filter(models.Post.id == id).first()
   if not post:
--- sqlite_app2/schemas.py
+++ sqlite_app2/schemas.py
@@ -2,29 +2,34 @@ from typing import List
 from pydantic import BaseModel
 
 class User(BaseModel):
-  id: int
   email: str
   password: str
-  class Config:
-    orm_mode = True
 
 class Post(BaseModel):
   title: str
   body: str
-  class Config:
-    orm_mode = True
+
+class CreatePost(BaseModel):
+  title: str
+  body: str
+  user_id: int
 
 class ShowUser(BaseModel):
   id: int
   email: str
-  posts: List[Post] = []
   class Config:
     orm_mode = True
 
 class ShowPost(BaseModel):
   title: str
   body: str
-  creator: ShowUser
-
   class Config:
     orm_mode = True
+
+class ShowUserWithPosts(ShowUser):
+  posts: List[ShowPost] = []
+
+class ShowPostWithUser(ShowPost):
+  title: str
+  body: str
+  creator: ShowUser
